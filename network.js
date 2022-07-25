385728690fc0a5f812cd9a956e8e2cc61f52e4ad
mauro sala <mauro.sala@gmail.com>
2022-07-25 09:42:52 +0200

    feat(network): proxy

--- network.js
+++ network.js
@@ -1,11 +1,23 @@
 const axios = require('axios')
 
+const params = {
+  proxy: {
+    host: 'localhost',
+    port: 3000
+  }
+}
+
+if (process.env.HTTP_PROXY) {
+  params.proxy.host = process.env.HTTP_PROXY.split(':')[0]
+  params.proxy.port = process.env.HTTP_PROXY.split(':')[1]
+}
+
 const main = async () => {
   const api =
     'https://api.github.com/repos/krateoplatformops/krateo-template-ted/contents/template.yaml'
 
   axios
-    .get(api)
+    .get(api, params)
     .then((res) => {
       console.log(res.data)
       console.log('Ok!')
